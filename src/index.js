f7b3ca62ab340f6d7594a70fd90e526ba96856ef
Adam Heim <jefffan24@gmail.com>
2014-09-12 08:32:53 -0400

    Add editorconfig and gitignore.  Reformat JS file to match editorconfig style

--- src/index.js
+++ src/index.js
@@ -1,54 +1,54 @@
-(function(window, undefined){
-  var PluginName = {
-    _defaults: {},
-    _options: {},
-    _events: {},
-    _listeners: {},
-    _sources: {},
+(function (window, undefined) {
+    var PluginName = {
+        _defaults: {},
+        _options: {},
+        _events: {},
+        _listeners: {},
+        _sources: {},
 
-    _addEvent: function(key, value){
-      this._events[key] = value;
-    },
-    _addListener: function(key, value){
-      this._events[key] = value;
-    },
-    _addSource: function(key, value){
-      this._sources[key] = value;
-    },
-    _setOptions: function(options){
+        _addEvent: function (key, value) {
+            this._events[key] = value;
+        },
+        _addListener: function (key, value) {
+            this._events[key] = value;
+        },
+        _addSource: function (key, value) {
+            this._sources[key] = value;
+        },
+        _setOptions: function (options) {
 
-    },
-    _setOption: function(name, value){
-      this._options[name] = value;
-    }
-  };
-
-  var log = function (message, error) {
-      message = message instanceof Array ? message : [message];
-      window.console && this.ajaxOptions.debug && (error ? console.error : console.log).apply(console, message);
-  };
-
-  var extend = function(obj){
-    var source, prop;
-    for(var i = 1, length = arguments.length; i < length; i++){
-      source = arguments[i];
-      for(prop in source){
-        if(Object.prototype.hasOwnProperty.call(source, prop)){
-          obj[prop] = source[prop];
+        },
+        _setOption: function (name, value) {
+            this._options[name] = value;
         }
-      }
-    }
-  };
+    };
 
-  var PublicPluginName = function(options){
-    if(arguments.length > 1){
-      // Attempting to set an option
-    } else if(arguments.length == 1){
+    var log = function (message, error) {
+        message = message instanceof Array ? message : [message];
+        window.console && this.ajaxOptions.debug && (error ? console.error : console.log).apply(console, message);
+    };
 
-    } else {
+    var extend = function (obj) {
+        var source, prop;
+        for (var i = 1, length = arguments.length; i < length; i++) {
+            source = arguments[i];
+            for (prop in source) {
+                if (Object.prototype.hasOwnProperty.call(source, prop)) {
+                    obj[prop] = source[prop];
+                }
+            }
+        }
+    };
 
-    }
-  };
+    var PublicPluginName = function (options) {
+        if (arguments.length > 1) {
+            // Attempting to set an option
+        } else if (arguments.length == 1) {
 
-  window.PluginName = PublicPluginName;
+        } else {
+
+        }
+    };
+
+    window.PluginName = PublicPluginName;
 })(window);
